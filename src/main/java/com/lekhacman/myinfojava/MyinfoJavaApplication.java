81aa40692a8b93ed5739f2c317a9a02419d6313c
Andrew Le <andrewlkm@spgroup.com.sg>
2020-03-22 12:05:41 +0700

    init project

---
+++ src/main/java/com/lekhacman/myinfojava/MyinfoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.lekhacman.myinfojava;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyinfoJavaApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(MyinfoJavaApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/lekhacman/myinfojava/MyinfoJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lekhacman.myinfojava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyinfoJavaApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
